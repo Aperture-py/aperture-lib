38865d935f783e9f4844a813ed22a2e785acc8ba
Alex Robbins <robbinsa@wit.edu>
2018-06-29 17:05:56 -0400

    Allow for resultant images to be returned w/ their requested resolution

--- aperturelib/__init__.py
+++ aperturelib/__init__.py
@@ -35,8 +35,25 @@ def format_image(path, options):
         1 unless resolutions for resizing are provided in the options. 
     '''
     image = Image.open(path)
-    image_pipeline_results = __pipeline_image(image, options)
+    image_pipeline_results = __pipeline_image(image, options, False)
     return image_pipeline_results
+
+
+def format_image_get_resolutions(path, options):
+    '''Formats an image.
+
+    Args:
+        path (str): Path to the image file.
+        options (dict): Options to apply to the image.
+
+    Returns:
+        (list) A list of tuples containing PIL images and w,h tuples 
+        representing resolutions. The list will always be of length
+        1 unless resolutions for resizing are provided in the options. 
+    '''
+    image = Image.open(path)
+    image_pipeline_results = __pipeline_image(image, options, True)
+    return image_pipeline_results    
 
 
 def save(image, out_file, **kwargs):
@@ -57,12 +74,13 @@ def save(image, out_file, **kwargs):
 # =========================
 
 
-def __pipeline_image(image, options):
+def __pipeline_image(image, options, ret_resolutions):
     '''Sends an image through a processing pipeline.
     Applies all (relevant) provided options to a given image.
     Args:
         image: An instance of a PIL Image.
         options: Options to apply to the image (i.e. resolutions).
+        ret_resolutions (bool): Whether formatted images should be returned with their resolutions (if resolutions were changed)
     Returns:
         A list containing instances of PIL Images. This list will always be length
         1 if no options exist that require multiple copies to be created for a single
@@ -73,7 +91,7 @@ def __pipeline_image(image, options):
     # Begin pipline
 
     # 1. Create image copies for each resolution
-
+    resolutions = []
     if 'resolutions' in options:
         resolutions = options['resolutions']  # List of resolution tuples
         for res in resolutions:
@@ -84,26 +102,33 @@ def __pipeline_image(image, options):
             results.append(img_rs)
 
     # 2. Apply watermark to each image copy
-    wtrmk_path = options['wmark-img']
-    if wtrmk_path:
-        if len(results) == 0:
-            image = watermark_image(image, wtrmk_path)  #watermark actual image?
-        else:
-            for i in range(0, len(results)):
-                results[i] = watermark_image(
-                    results[i], wtrmk_path)  #watermark actual image
+    if 'wmark-img' in options:
+        wtrmk_path = options['wmark-img']
+        if wtrmk_path:
+            if len(results) == 0:
+                image = watermark_image(image, wtrmk_path)  #watermark actual image?
+            else:
+                for i in range(0, len(results)):
+                    results[i] = watermark_image(
+                        results[i], wtrmk_path)  #watermark actual image
 
-    wtrmk_txt = options['wmark-txt']
-    if wtrmk_txt:
-        if len(results) == 0:
-            image = watermark_text(image, wtrmk_txt)  #watermark actual image?
-        else:
-            for i in range(0, len(results)):
-                results[i] = watermark_text(results[i],
-                                            wtrmk_txt)  #watermark actual image
+    if 'wmark-txt' in options:
+        wtrmk_txt = options['wmark-txt']
+        if wtrmk_txt:
+            if len(results) == 0:
+                image = watermark_text(image, wtrmk_txt)  #watermark actual image?
+            else:
+                for i in range(0, len(results)):
+                    results[i] = watermark_text(results[i],
+                                                wtrmk_txt)  #watermark actual image
 
     # Fallback: Nothing was done to the image
     if len(results) == 0:
         results.append(image)
+    
+    if ret_resolutions:
+        if resolutions:
+            if len(resolutions) == len(results):
+                results = list(zip(results, resolutions))
 
     return results
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
-__version__ = '0.0.0.dev8'
+__version__ = '0.0.0.dev9'
 
 setup(
     name='aperturelib',
